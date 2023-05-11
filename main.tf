d4d455fe15a3b332d9b07276757ec1efd3f62ed0
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-05-11 09:58:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-southeast-1" # replace with the desired region
+}
+
+resource "aws_cognito_user" "example_user" {
+  user_pool_id = "ap-southeast-1_Rk5e4SIej"
+  username     = var.username
+  message_action = "SUPPRESS"
+  # set permanent password for user
+  password = var.password
+}
